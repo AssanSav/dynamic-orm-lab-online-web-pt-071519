8b9036f667b306b48e77900c67c8997a19215eec
assane savadogo <assane.savadogo81@gmail.com>
2019-10-02 15:25:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -44,10 +44,10 @@ class InteractiveRecord
   end
   
   def self.find_by(attr_hash)
-    c = attr_hash.keys.join
-    binding.pry
+    # c = attr_hash.keys.join
+    # binding.pry
   #condition = attr_hash.tap {|c| "#{c.to_s}= ?"}.join("AND") 
-    #DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{} = ?", attr_hash.values)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{attr_hash.keys.join} = ?", attr_hash.values)
     
   end
 end
